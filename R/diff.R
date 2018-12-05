6c29f892dec17a2dd2b971ad64b86931a52809ec
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-12-05 13:10:23 -0500

    generate loop for every possible reference witness

--- R/diff.R
+++ R/diff.R
@@ -23,35 +23,45 @@ lv_wrapper <- function(x, weight) {
     mutate_at(vars(source, target), funs(factor(., levels = names(x))))
 }
 
+active_weight <- function() 1
+inactive_weight <- function() 0.0001
+
 deletions <- function(x) {
-  lv_wrapper(x, weight = c(d = 1, i = 0.001, s = 0.0001, t = 0.0001))
+  lv_wrapper(x, weight = c(d = active_weight(), 
+                           i = inactive_weight(), 
+                           s = inactive_weight(), 
+                           t = inactive_weight()
+  )
+  )
 }
 
 additions <- function(x) {
-  lv_wrapper(x, weight = c(d = 0.0001, i = 1, s = 0.0001, t = 0.0001))
+  lv_wrapper(x, weight = c(d = inactive_weight(), 
+                           i = active_weight(), 
+                           s = inactive_weight(), 
+                           t = inactive_weight()
+  )
+  )
 }
 
 substitutions <- function(x) {
-  lv_wrapper(x, weight = c(d = 0.0001, i = 0.0001, s = 1, t = 0.0001))
+  lv_wrapper(x, weight = c(d = inactive_weight(), 
+                           i = inactive_weight(), 
+                           s = active_weight(), 
+                           t = inactive_weight()
+  )
+  )
 }
 
-pairwise_app_comparison <- function(ordered_apps) {
+pairwise_app_comparison <- function(ordered_apps, diff_function) {
+  witnesses <- levels(ordered_apps$witness)
+  
   split_apps <- ordered_apps %>% 
-    split(.$app_ids)
+    group_by(app) %>% 
+    by_slice(~diff_function(set_names(.$text, .$witness)), .collate = "rows")
   
-  contents <- map(split_apps, function(x) {
-    res <- x$content
-    set_names(res, x$witness_id)
-  })
-  
-  df_deletions <- map_df(contents, deletions, .id = "app_id")
-  df_additions <- map_df(contents, additions, .id = "app_id")
-  df_substitutions <- map_df(contents, substitutions, .id = "app_id")
-
-  df_deletions %>% 
-    rename("deletions" = value) %>% 
-    left_join(rename(df_additions, "additions" = value), by = c("app_id", "source", "target")) %>% 
-    left_join(rename(df_substitutions, "substitutions" = value), by = c("app_id", "source", "target"))
+  witnessed_apps <- split_apps %>% 
+    mutate_at(vars(source, target), funs(factor(witnesses[.], levels = witnesses, ordered = TRUE)))
 }
 
 app_features <- function(reshaped_spine) {
@@ -81,33 +91,38 @@ app_page_plot <- function(app_pages) {
     scale_fill_viridis_c(na.value = "#2E0A40")
 }
 
-synoptic_app_page_build <- function(ordered_apps, pariwise_app_differences, reference_witness = "fThomas") {
-  source_differences <- pariwise_app_differences %>% 
+synoptic_app_page_build <- function(ordered_apps, pairwise_app_differences, reference_witness, ...) {
+  source_differences <- pairwise_app_differences %>% 
     filter(source == reference_witness)
   
   plot_list <- map(levels(source_differences$target), function(x) {
     target_app_contents <- ordered_apps %>% 
-      filter(witness_id == x)
+      filter(witness == x)
     
     target_distances <- source_differences %>% 
       filter(target == x)
     
-    single_diff_plot(target_app_contents, target_distances)
+    single_diff_plot(target_app_contents, target_distances, ...)
   })
-  
+}
+
+floorless <- function(x) {
+  cutoff <- names(table(x))[2]
+  if_else(x <= cutoff, 0, x)
 }
 
 single_diff_plot <- function(target_app_contents, target_distances, ...) {
-  bound_apps <- target_app_contents %>% 
-    left_join(target_distances, by = c("app_ids" = "app_id")) %>% 
-    rename(text = content) %>% 
-    mutate_at(vars(additions, deletions), log) %>% 
-    mutate_at(vars(additions, deletions), funs(scales::rescale(if_else(is.infinite(.), NA_real_, .)))) %>% 
-    mutate(composite = additions - deletions,
-           magnitude = log(additions + deletions)) %>% 
-    mutate_at(vars(magnitude), funs(coalesce(., -0.1)))
+  source <- first(target_distances$source)
+  target <- first(target_distances$target)
   
-  min(bound_apps$magnitude)
+  bound_apps <- target_app_contents %>% 
+    left_join(target_distances, by = "app") %>% 
+    mutate(transformed_value = floorless(rescale(log10(value + 1)))) %>% 
+    select(-value) %>% 
+    spread(edit_type, transformed_value) %>% 
+    mutate(composite = char_additions - char_deletions,
+           magnitude = char_additions + char_deletions,
+           composite = if_else(magnitude == 0, NA_real_, composite))
   
   unbound_apps <- unnest_tokens(bound_apps, output = word, input = text)
   
@@ -116,5 +131,6 @@ single_diff_plot <- function(target_app_contents, target_distances, ...) {
   
   ggpage_plot(page_layout, aes(fill = composite, alpha = magnitude), paper.limits = 0.1) + 
     scale_fill_gradient2(low = "red", mid = "yellow", high = "green") +
-    scale_alpha_continuous(range = c(0.1, 1))
+    scale_alpha_continuous(range = c(0.5, 1)) +
+    ggtitle(str_glue("{source} -> {target}"))
 }
--- drake.R
+++ drake.R
@@ -6,12 +6,14 @@ pkgconfig::set_config(
 
 library(drake)
 library(tidyverse)
+library(purrrlyr)
 library(xml2)
 library(fs)
 library(rematch2)
 library(stringdist)
 library(tidytext)
 library(ggpage)
+library(scales)
 
 # Load all functions
 dir_walk("R", source)
@@ -20,11 +22,26 @@ dir_walk("R", source)
 
 collation_dir <- "../fv-postCollation/P1-output/"
 collationfiles <- dir_ls(collation_dir, glob = "*.xml")
+witnesses <- map(collationfiles, function(x) {
+  read_xml(x) %>% xml_find_all(".//d1:rdg") %>% xml_attr("wit")
+}) %>% flatten_chr() %>% unique()
 
 fv_plan <- drake_plan(
   full_df = map_df(collationfiles, read_P1),
   ordered_apps = order_apps(full_df),
-  pariwise_app_differences = pairwise_app_comparison(ordered_apps),
-  app_pages = app_page_build(app_attributes, ncol = 1),
-  app_page_plot(app_pages)
+  pairwise_additions = pairwise_app_comparison(ordered_apps, additions),
+  pairwise_deletions = pairwise_app_comparison(ordered_apps, deletions),
+  pairwise_app_differences = bind_rows("char_additions" = pairwise_additions,
+                               "char_deletions" = pairwise_deletions,
+                               .id = "edit_type")
 )
+
+witness_plots_generic <- drake_plan(diffplot = synoptic_app_page_build(ordered_apps, pairwise_app_differences, reference_witness = "wit__"))
+witness_plots_plan <- evaluate_plan(witness_plots_generic, wildcard = "wit__", values = witnesses)
+
+fv_plan <- bind_plans(
+  fv_plan, 
+  witness_plots_plan)
+
+make(fv_plan)
+
