17524c2acc11e0f59fb3dec399c89edfd2c1f1a4
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-12-06 13:26:49 -0500

    move exclusively-plotting related functions into a separate file
    closes #10

--- R/diff.R
+++ R/diff.R
@@ -1,3 +1,5 @@
+# Tidy raw xml outputs ----
+
 order_apps <- function(full_df) {
   full_df %>% 
     bind_re_match(app, pattern = "^C(?<chunk>\\d{2})(?<subchunk>[a-z])?_app(?<index>\\d+)") %>% 
@@ -9,6 +11,24 @@ order_apps <- function(full_df) {
     arrange(chunk, subchunk, index, witness) %>% 
     mutate(app = factor(app, levels = unique(app), ordered = TRUE))
 }
+
+# Max edit calculations ----
+
+app_features <- function(reshaped_spine) {
+  reshaped_spine %>% 
+    group_by(app_ids) %>% 
+    mutate(
+      content_char = nchar(content),
+      witness_difference = coalesce(stringdist(content, lag(content)))) %>% 
+    summarize(
+      text = last(content),
+      app_length = max(content_char),
+      max_app_edit_distance = max(witness_difference, na.rm = TRUE),
+      log_max_app_edit_distance = log10(if_else(max_app_edit_distance == 0, NA_real_, max_app_edit_distance))
+    )
+}
+
+# Addition/Deletion calculations ----
 
 lv_wrapper <- function(x, weight) {
   res <- stringdistmatrix(x, x, method = "lv", weight = weight)
@@ -64,48 +84,6 @@ pairwise_app_comparison <- function(ordered_apps, diff_function) {
     mutate_at(vars(source, target), funs(factor(witnesses[.], levels = witnesses, ordered = TRUE)))
 }
 
-app_features <- function(reshaped_spine) {
-  reshaped_spine %>% 
-    group_by(app_ids) %>% 
-    mutate(
-      content_char = nchar(content),
-      witness_difference = coalesce(stringdist(content, lag(content)))) %>% 
-    summarize(
-      text = last(content),
-      app_length = max(content_char),
-      max_app_edit_distance = max(witness_difference, na.rm = TRUE),
-      log_max_app_edit_distance = log10(if_else(max_app_edit_distance == 0, NA_real_, max_app_edit_distance))
-    )
-}
-
-app_page_build <- function(app_attributes, ...) {
-  un_apps <- app_attributes %>% 
-    unnest_tokens(output = word, input = text)
-  
-  ggpage_build(un_apps$word, ...) %>% 
-    bind_cols(un_apps) 
-}
-
-app_page_plot <- function(app_pages) {
-  ggpage_plot(app_pages, aes(fill = log_max_app_edit_distance)) +
-    scale_fill_viridis_c(na.value = "#2E0A40")
-}
-
-synoptic_app_page_build <- function(ordered_apps, pairwise_app_differences, reference_witness, ...) {
-  source_differences <- pairwise_app_differences %>% 
-    filter(source == reference_witness)
-  
-  plot_list <- map(levels(source_differences$target), function(x) {
-    target_app_contents <- ordered_apps %>% 
-      filter(witness == x)
-    
-    target_distances <- source_differences %>% 
-      filter(target == x)
-    
-    single_diff_ggpage(target_app_contents, target_distances, ...)
-  })
-}
-
 floorless <- function(x) {
   cutoff <- names(table(x))[2]
   if_else(x <= cutoff, 0, x)
@@ -120,60 +98,4 @@ composite_changes <- function(contents, distances) {
     mutate(composite = char_additions - char_deletions,
            magnitude = char_additions + char_deletions,
            composite = if_else(magnitude == 0, NA_real_, composite))
-}
-
-single_diff_ggpage <- function(target_app_contents, target_distances, ...) {
-  source <- first(target_distances$source)
-  target <- first(target_distances$target)
-  
-  bound_apps <- composite_changes(target_app_contents, target_distances)
-  
-  unbound_apps <- unnest_tokens(bound_apps, output = word, input = text)
-  
-  page_layout <- ggpage_build(bound_apps, ...) %>% 
-    bind_cols(unbound_apps)
-  
-  ggpage_plot(page_layout, aes(fill = composite, alpha = magnitude), paper.limits = 0.1) + 
-    scale_fill_gradient2(low = "green", mid = "yellow", high = "red") +
-    scale_alpha_continuous(range = c(0.5, 1)) +
-    ggtitle(str_glue("{source} -> {target}")) +
-    theme(legend.position = "none")
-}
-
-heatmap_df <- function(ordered_apps, pairwise_app_differences, source_witness, target_witness) {
-  target_contents <- ordered_apps %>% 
-    filter(witness == target_witness)
-  
-  selected_differences <- pairwise_app_differences %>% 
-    filter(source == source_witness, target == target_witness)
-  
-  expanded_content_measures <- composite_changes(target_contents, selected_differences) %>% 
-    group_by(app) %>% 
-    by_slice(function(x) {
-      nreps <- nchar(x$text)
-      tibble(
-        app_index = seq_len(nreps),
-        composite = x$composite,
-        magnitude = x$magnitude)
-    }, .collate = "rows") %>% 
-    arrange(app) %>% 
-    mutate(index = row_number()) %>% 
-    mutate(source = source_witness, target = target_witness)
-}
-
-heatmap_plot <- function(heatmap_df) {
-  ggplot(expanded_content_measures, aes(x = 1, y = index, fill = composite, alpha = magnitude)) + 
-    geom_raster() +
-    scale_fill_gradient2(low = "red", mid = "yellow", high = "green", na.value = "gray30") +
-    scale_alpha_continuous(range = c(0.5, 1)) +
-    theme_minimal()
-  
-  
-  ggplot(compiled_df, aes(x = source, y = index, fill = composite, alpha = magnitude)) + 
-    facet_wrap(~ target, ncol = 1, scales = "free_y") + 
-    geom_raster() +
-    scale_fill_gradient2(low = "green", mid = "yellow", high = "red", na.value = "gray30") +
-    scale_alpha_continuous(range = c(0.1, 1)) +
-    theme_minimal()
-  
 }
---
+++ R/plotting.R
@@ -0,0 +1,88 @@
+# ggpage visualization - most-edited apps
+
+app_page_build <- function(app_attributes, ...) {
+  un_apps <- app_attributes %>% 
+    unnest_tokens(output = word, input = text)
+  
+  ggpage_build(un_apps$word, ...) %>% 
+    bind_cols(un_apps) 
+}
+
+app_page_plot <- function(app_pages) {
+  ggpage_plot(app_pages, aes(fill = log_max_app_edit_distance)) +
+    scale_fill_viridis_c(na.value = "#2E0A40")
+}
+
+# ggpage visualization - reference comparisons ----
+
+synoptic_app_page_build <- function(ordered_apps, pairwise_app_differences, reference_witness, ...) {
+  source_differences <- pairwise_app_differences %>% 
+    filter(source == reference_witness)
+  
+  plot_list <- map(levels(source_differences$target), function(x) {
+    target_app_contents <- ordered_apps %>% 
+      filter(witness == x)
+    
+    target_distances <- source_differences %>% 
+      filter(target == x)
+    
+    single_diff_ggpage(target_app_contents, target_distances, ...)
+  })
+}
+
+single_diff_ggpage <- function(target_app_contents, target_distances, ...) {
+  source <- first(target_distances$source)
+  target <- first(target_distances$target)
+  
+  bound_apps <- composite_changes(target_app_contents, target_distances)
+  
+  unbound_apps <- unnest_tokens(bound_apps, output = word, input = text)
+  
+  page_layout <- ggpage_build(bound_apps, ...) %>% 
+    bind_cols(unbound_apps)
+  
+  ggpage_plot(page_layout, aes(fill = composite, alpha = magnitude), paper.limits = 0.1) + 
+    scale_fill_gradient2(low = "green", mid = "yellow", high = "red") +
+    scale_alpha_continuous(range = c(0.5, 1)) +
+    ggtitle(str_glue("{source} -> {target}")) +
+    theme(legend.position = "none")
+}
+
+heatmap_df <- function(ordered_apps, pairwise_app_differences, source_witness, target_witness) {
+  target_contents <- ordered_apps %>% 
+    filter(witness == target_witness)
+  
+  selected_differences <- pairwise_app_differences %>% 
+    filter(source == source_witness, target == target_witness)
+  
+  expanded_content_measures <- composite_changes(target_contents, selected_differences) %>% 
+    group_by(app) %>% 
+    by_slice(function(x) {
+      nreps <- nchar(x$text)
+      tibble(
+        app_index = seq_len(nreps),
+        composite = x$composite,
+        magnitude = x$magnitude)
+    }, .collate = "rows") %>% 
+    arrange(app) %>% 
+    mutate(index = row_number()) %>% 
+    mutate(source = source_witness, target = target_witness)
+}
+
+heatmap_plot <- function(heatmap_df) {
+  ggplot(expanded_content_measures, aes(x = 1, y = index, fill = composite, alpha = magnitude)) + 
+    geom_raster() +
+    scale_fill_gradient2(low = "red", mid = "yellow", high = "green", na.value = "gray30") +
+    scale_alpha_continuous(range = c(0.5, 1)) +
+    theme_minimal()
+  
+  
+  ggplot(compiled_df, aes(x = source, y = index, fill = composite, alpha = magnitude)) + 
+    facet_wrap(~ target, ncol = 1, scales = "free_y") + 
+    geom_raster() +
+    scale_fill_gradient2(low = "green", mid = "yellow", high = "red", na.value = "gray30") +
+    scale_alpha_continuous(range = c(0.1, 1)) +
+    theme_minimal()
+  
+}
+
