bc24a7e10ddf58b931e18e824484e4eae2d85af8
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-06-12 16:33:10 -0400

    show popup text

--- d3/app.js
+++ d3/app.js
@@ -25,7 +25,7 @@
 
   function draw_apps(data) {
     var maxNchar = d3.max(data, d => d3.max([d.fMS_nchar, d.f1818_nchar, d.f1823_nchar, d.f1831_nchar, d.fThomas_nchar]))
-    var width_scale = d3.scale.linear().domain([0, maxNchar]).range([0, 500])
+    var width_scale = d3.scale.linear().domain([0, maxNchar]).range([0, 800])
 
     var minchange = d3.min(data, d => d3.min([d.f1818_f1818, d.f1818_f1823, d.f1818_f1831, d.f1818_fMS, d.f1818_fThomas, d.f1823_f1818, d.f1823_f1823, d.f1823_f1831, d.f1823_fMS, d.f1823_fThomas, d.f1831_f1818, d.f1831_f1823, d.f1831_f1831, d.f1831_fMS, d.f1831_fThomas, d.fMS_f1818, d.fMS_f1823, d.fMS_f1831, d.fMS_fMS, d.fMS_fThomas, d.fThomas_f1818, d.fThomas_f1823, d.fThomas_f1831, d.fThomas_fMS, d.fThomas_fThomas]))
     var maxchange = d3.max(data, d => d3.max([d.f1818_f1818, d.f1818_f1823, d.f1818_f1831, d.f1818_fMS, d.f1818_fThomas, d.f1823_f1818, d.f1823_f1823, d.f1823_f1831, d.f1823_fMS, d.f1823_fThomas, d.f1831_f1818, d.f1831_f1823, d.f1831_f1831, d.f1831_fMS, d.f1831_fThomas, d.fMS_f1818, d.fMS_f1823, d.fMS_f1831, d.fMS_fMS, d.fMS_fThomas, d.fThomas_f1818, d.fThomas_f1823, d.fThomas_f1831, d.fThomas_fMS, d.fThomas_fThomas]))
@@ -74,11 +74,31 @@
       return
     }
 
+    function app_hover(d) {
+      d3.select(this)
+        .classed("hovered", true)
+        .append("div")
+        .classed("tt", true)
+        .html(t => t[get_target_witness() + "_text"])
+    }
+
+    function app_nohover(d) {
+      d3.select(this)
+        .classed("hovered", false)
+        .select("div.tt")
+        .remove()
+    }
+
     d3.selectAll("#source-witness button")
       .on("click", source_input_click);
 
     d3.selectAll("#target-witness button")
       .on("click", target_input_click);
+
+    var legend_color = d3.legend.color()
+      .shapeWidth(30)
+      .orient('horizontal')
+      .scale(col_scale);
 
     d3.select("#wrapper")
       .selectAll("div.app")
@@ -89,10 +109,22 @@
       .attr("app", d => d.app)
       .style("width", d => width_scale(d[get_source_witness() + "_nchar"]) + "px")
 
-    update_heading(get_source_witness(), get_target_witness())
+    d3.select("#wrapper")
+      .selectAll("div.app")
+      .on("mouseover", app_hover)
+      .on("mouseout", app_nohover)
+
+    input_change(get_source_witness(), get_target_witness())
+
+    d3.select("svg#legend")
+      .append("g")
+      .attr("class", "legend")
+      .attr("transform", "translate(20,20)")
+
+    d3.select("svg#legend")
+      .select(".legend")
+      .call(legend_color);
   };
-
-
 
   d3.json("data.json", function (error, data) {
     if (error) {
