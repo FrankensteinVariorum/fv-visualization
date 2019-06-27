ff78a27d3d4f0c0d991504b4393b47015fccda86
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-06-27 13:16:28 -0400

    restyle to use margin, not border, to separate blocks

--- d3/opcode_witnesses/app.js
+++ d3/opcode_witnesses/app.js
@@ -27,7 +27,7 @@
 
   function draw_apps(data) {
     var maxNchar = data.stats.nchar.max
-    var width_scale = d3.scale.linear().domain([0, maxNchar]).range([0, 300])
+    var width_scale = d3.scale.linear().domain([0, maxNchar]).range([0, 200])
 
     var maxadd = data.stats.addition.max
     var maxdel = data.stats.deletion.max
